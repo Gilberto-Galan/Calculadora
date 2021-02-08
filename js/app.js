90d55313b53333afd840122df42a516a430066c7
Gilberto Galán <71560171+Gilberto-Galan@users.noreply.github.com>
2021-02-08 14:39:50 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,39 @@
+const botonNumeros = document.getElementsByName('data-number');
+const botonOpera = document.getElementsByName('botonOpera');
+const botonIgual = document.getElementsByName('data-igual')[0];
+const botonDelete = document.getElementsByName('data-delete')[0];
+var result = document.getElementById('result');
+var opeActual = '';
+var opeAnterior = '';
+var operacion = undefined;
+
+botonNumeros.forEach(function(boton){
+    boton.addEventListener('click', function(){
+        agregarNumero(boton.innerText);
+    })
+});
+
+botonOpera.forEach(function(boton){
+    boton.addEventListener('click', function(){
+        selectOperacion(boton.innerText);        
+    })
+});
+
+botonIgual.addEventListener('click', function(){
+    calcular();
+    actualizarDisplay();
+});
+
+botonDelete.addEventListener('click', function(){
+    clear();
+    actualizarDisplay();
+});
+
+function agregarNumero(num);{
+    opeActual = opeActual.toString() + num.toString;
+    actualizarDisplay();
+}
+
+function actualizarDisplay(){
+    result.value = openActual;
+}
